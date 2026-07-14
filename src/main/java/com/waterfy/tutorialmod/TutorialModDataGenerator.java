00618c01379b073ede51a8e89f38262b4cad3a33
waterfy <rkaltnekar@gmail.com>
2026-07-14 23:29:31 +0200

    Add Loot Tables and Tags For Blocks

--- src/main/java/com/waterfy/tutorialmod/TutorialModDataGenerator.java
+++ src/main/java/com/waterfy/tutorialmod/TutorialModDataGenerator.java
@@ -1,5 +1,7 @@
 package com.waterfy.tutorialmod;
 
+import com.waterfy.tutorialmod.datagen.ModBlockLootTableProvider;
+import com.waterfy.tutorialmod.datagen.ModBlockTagsProvider;
 import com.waterfy.tutorialmod.datagen.ModModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
@@ -7,8 +9,10 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 public class TutorialModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-	var pack = fabricDataGenerator.createPack();
+		var pack = fabricDataGenerator.createPack();
 
-	pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModBlockTagsProvider::new);
+		pack.addProvider(ModBlockLootTableProvider::new);
 	}
 }
